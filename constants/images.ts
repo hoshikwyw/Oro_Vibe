b091d93abc36e4ae52cff50d9f2cb5db04b04910
hoshikwyw <khaingwutyiwin1712@gmail.com>
2025-02-15 22:13:55 +0630

    Project set up

---
+++ constants/images.ts
@@ -0,0 +1,27 @@
+import onboarding from "@/assets/images/onboarding.png";
+import avatar from "@/assets/images/avatar.png";
+import newYork from "@/assets/images/new-york.png";
+import japan from "@/assets/images/japan.png";
+import cardGradient from "@/assets/images/card-gradient.png";
+import barChart from "@/assets/images/bar-chart.png";
+import whiteGradient from "@/assets/images/white-gradient.png";
+import map from "@/assets/images/map.png";
+import noResult from "@/assets/images/no-result.png";
+import introImg from "@/assets/images/intro.png";
+import introIcon from "@/assets/images/intro-icon.png";
+import oroFavicon from '@/assets/images/OroFavicon.png';
+
+export default {
+  onboarding,
+  avatar,
+  newYork,
+  japan,
+  cardGradient,
+  barChart,
+  whiteGradient,
+  map,
+  noResult,
+  introImg,
+  introIcon,
+  oroFavicon,
+};
